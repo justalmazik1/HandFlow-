7c9d10c808c7c22d5fb3fb804b1cb12c453a6ec0
justalmazik1 <imalmazik1144@gmail.com>
2026-08-13 16:09:14 +0000

    Add HeldItemRenderer mixin

---
+++ src/main/java/com/example/mixin/HeldItemRendererMixin.java
@@ -0,0 +1,35 @@
+package com.example.mixin;
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HeldItemRenderer.class)
+public class HeldItemRendererMixin {
+    @Inject(
+        method = "renderFirstPersonItem",
+        at = @At("HEAD")
+    )
+    private void onRenderFirstPersonItem(
+        AbstractClientPlayerEntity player,
+        float tickDelta,
+        float pitch,
+        Hand hand,
+        float swingProgress,
+        ItemStack item,
+        float equipProgress,
+        MatrixStack matrices,
+        VertexConsumerProvider vertexConsumers,
+        int light,
+        CallbackInfo ci
+    ) {
+        matrices.translate(0.0D, -0.05D, 0.02D);
+    }
+}
