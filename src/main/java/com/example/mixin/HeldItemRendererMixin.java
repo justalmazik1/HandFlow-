e6c93c435749891c80717fbe26797bf494896ada
justalmazik1 <imalmazik1144@gmail.com>
2026-08-13 16:48:11 +0000

    Use safe HeldItemRenderer injection

--- src/main/java/com/example/mixin/HeldItemRendererMixin.java
+++ src/main/java/com/example/mixin/HeldItemRendererMixin.java
@@ -5,19 +5,28 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
-public class HeldItemRendererMixin {
+public abstract class HeldItemRendererMixin {
+
+    @Shadow
+    protected abstract void renderArmHoldingItem(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float equipProgress, float swingProgress, Arm arm);
+
     @Inject(
         method = "renderFirstPersonItem",
-        at = @At("HEAD")
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/render/item/HeldItemRenderer;renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V"
+        )
     )
-    private void onRenderFirstPersonItem(
+    private void onRenderItem(
         AbstractClientPlayerEntity player,
         float tickDelta,
         float pitch,
@@ -30,6 +39,7 @@ public class HeldItemRendererMixin {
         int light,
         CallbackInfo ci
     ) {
-        matrices.translate(0.0D, -0.05D, 0.02D);
+        Arm arm = (hand == Hand.MAIN_HAND) ? player.getMainArm() : player.getMainArm().getOpposite();
+        this.renderArmHoldingItem(matrices, vertexConsumers, light, equipProgress, swingProgress, arm);
     }
 }
