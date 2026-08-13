92dd1982a2440332c7e4d59b541ee5ff4dfdf034
justalmazik1 <imalmazik1144@gmail.com>
2026-08-13 16:28:09 +0000

    Single-source Fabric layout

--- src/main/java/com/example/mixin/HeldItemRendererMixin.java
+++ src/main/java/com/example/mixin/HeldItemRendererMixin.java
@@ -5,20 +5,14 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Arm;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
-public abstract class HeldItemRendererMixin {
-
-    @Shadow
-    protected abstract void renderArmHoldingItem(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, float equipProgress, float swingProgress, Arm arm);
-
+public class HeldItemRendererMixin {
     @Inject(
         method = "renderFirstPersonItem",
         at = @At("HEAD")
@@ -36,13 +30,6 @@ public abstract class HeldItemRendererMixin {
         int light,
         CallbackInfo ci
     ) {
-        Arm arm = (hand == Hand.MAIN_HAND) ? player.getMainArm() : player.getMainArm().getOpposite();
-
-        if (!item.isEmpty()) {
-            matrices.push();
-            this.renderArmHoldingItem(matrices, vertexConsumers, light, equipProgress, swingProgress, arm);
-            matrices.pop();
-        }
+        matrices.translate(0.0D, -0.05D, 0.02D);
     }
 }
-
